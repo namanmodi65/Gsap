3cb4ea53cdb6d59e1492adb1d49158ee2476404a
Naman Jain <103953760+namanmodi65@users.noreply.github.com>
2025-07-03 22:29:37 +0530

    art add

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import React from 'react'
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Cocktails from './components/Cocktails';
+import About from './components/About';
+import Art from './components/Art';
 
 gsap.registerPlugin(ScrollTrigger,SplitText);
 
@@ -13,6 +15,8 @@ const App = () => {
       <Navbar/>
       <Hero/>
       <Cocktails/>
+      <About/>
+      <Art/>
     </main>
   )
 }
